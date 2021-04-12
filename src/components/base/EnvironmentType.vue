89751203b9f3733e49b9cc01f82e97d7cb50888d
Pettyfer <pettyferlove@live.cn>
2021-04-12 16:17:30 +0800

    :wrench: 增加环境类型组件

---
+++ src/components/base/EnvironmentType.vue
@@ -0,0 +1,86 @@
+<template>
+  <span>
+    <v-select
+      v-if="type === 'select'"
+      v-model="childValue"
+      :items="environmentTypes"
+      item-text="name"
+      item-value="value"
+      label="环境类型"
+      prepend-icon="mdi-wrench"
+      @change="changeValue"
+    ></v-select>
+    <template v-else>
+      <v-chip color="primary" outlined v-if="childValue === 1" class="ma-2">
+        开发环境
+      </v-chip>
+      <v-chip color="green" outlined v-if="childValue === 2" class="ma-2">
+        测试环境
+      </v-chip>
+      <v-chip color="orange" outlined v-if="childValue === 9" class="ma-2">
+        生产环境
+      </v-chip>
+    </template>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "EnvironmentType",
+  props: {
+    value: [Number],
+    type: {
+      type: String,
+      default: "select",
+    },
+  },
+  watch: {
+    value() {
+      this.initValue();
+    },
+  },
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  data: () => {
+    return {
+      childValue: "",
+      isInit: false,
+      environmentTypes: [
+        {
+          name: "开发环境",
+          value: 1,
+        },
+        {
+          name: "测试环境",
+          value: 2,
+        },
+        {
+          name: "生产环境",
+          value: 9,
+        },
+      ],
+    };
+  },
+  methods: {
+    initValue() {
+      this.isInit = true;
+      this.childValue = this.value;
+      this.isInit = false;
+    },
+    changeValue(value) {
+      if (!this.isInit) {
+        this.$emit("change", value);
+      }
+    },
+  },
+  mounted() {
+    if (this.value) {
+      this.initValue();
+    }
+  },
+};
+</script>
+
+<style scoped></style>
